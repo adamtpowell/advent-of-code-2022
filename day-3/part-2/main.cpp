db8803d210a306f86a50e9c822a13642075ea011
Adam Tassara Powell <apowell@umass.edu>
2022-12-03 23:00:28 -0500

    Add notes for what to work on tommorow

--- day-3/part-2/main.cpp
+++ day-3/part-2/main.cpp
@@ -6,6 +6,48 @@
 #include <set>
 using namespace std;
 
+/*
+Lessons learned:
+
+Traditional iterators and stuff are verbose
+
+stl is deeper than I thought
+
+look into the ranges library for good FP.
+
+Think like I'd do it in a dynamic language.
+
+The other way to go is with bitwise math, which I also want to do if I can go for speed.
+
+
+CHECK THIS OUT:
+
+#include <ranges>
+#include <iostream>
+
+int main()
+{
+    auto const ints = {0,1,2,3,4,5};
+    auto even = [](int i) { return 0 == i % 2; };
+    auto square = [](int i) { return i * i; };
+
+    // "pipe" syntax of composing the views:
+    for (int i : ints | std::views::filter(even) | std::views::transform(square)) {
+        std::cout << i << ' ';
+    }
+
+    std::cout << '\n';
+
+    // a traditional "functional" composing syntax:
+    for (int i : std::views::transform(std::views::filter(ints, even), square)) {
+        std::cout << i << ' ';
+    }
+}
+
+WILD
+
+*/
+
 char getNextBadge() {
 
     string line1;
